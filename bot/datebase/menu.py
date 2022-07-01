02abee656744ccf064039c55ad5d957b758974f5
MelekhJury <whitegold4864@gmail.com>
2022-07-02 00:31:58 +0300

    Изменение файла меню в файл ДЗ

--- bot/datebase/menu.py
+++ bot/datebase/menu.py
@@ -1,15 +1,12 @@
 import sqlite3
 
-
-
-
 def SelectTable(dbpath):
     try:
         sqlite_connection = sqlite3.connect(dbpath)
         cursor = sqlite_connection.cursor()
         print('Соединение с базой данных прошло успешно.')
 
-        sqlite_selection_query = "SELECT * FROM menu;"
+        sqlite_selection_query = "SELECT * FROM done_homework;"
         cursor.execute(sqlite_selection_query)
         record = cursor.fetchall()
         cursor.close()
@@ -27,34 +24,13 @@ def selectFromNames(name,dbpath):
         cursor = sqlite_connection.cursor()
         print('Соединение с базой данных прошло успешно.')
 
-        sqlite_selection_query = "SELECT * FROM menu WHERE name=?;"
+        sqlite_selection_query = "SELECT * FROM done_homework WHERE name=?;"
         cursor.execute(sqlite_selection_query,(name,))
         record = cursor.fetchall()
         cursor.close()
         return record
     except sqlite3.Error as error:
-        print("Не удалось выбрать данные по именам поситителей.", error)
-    finally:
-        if sqlite_connection:
-            sqlite_connection.close()
-            print("Соединение с SQLite закрыто")
-
-
-def maxID(dbpath):
-    try:
-        sqlite_connection = sqlite3.connect(dbpath)
-        cursor = sqlite_connection.cursor()
-        print('Соединение с базой данных прошло успешно.')
-
-        sqlite_selection_query = "SELECT MAX(id) FROM menu;"
-        cursor.execute(sqlite_selection_query)
-        record = cursor.fetchone()
-        cursor.close()
-        if record[0] == None:
-            return 0
-        return record[0]
-    except sqlite3.Error as error:
-        print("Не удалось выбрать данные из таблицы.", error)
+        print("Не удалось выбрать данные по именам студентов.", error)
     finally:
         if sqlite_connection:
             sqlite_connection.close()
@@ -62,17 +38,20 @@ def maxID(dbpath):
 
 
 
-def addNewProduct(record: list,dbpath):
+
+
+
+def addNewHomework(record: list,dbpath):
     print()
     try:
         sqlite_connection = sqlite3.connect(dbpath)
         cursor = sqlite_connection.cursor()
         print('Соединение с базой данных прошло успешно.')
 
-        insert_query = '''INSERT INTO menu (name,photo,type,price,ingridients)
-                          VALUES (?,?,?,?,?);'''
+        insert_query = '''INSERT INTO done_homework (name,photo)
+                          VALUES (?,?);'''
         cursor.executemany(insert_query,(record,))
-        print('Запись добавленна')
+        print('Домашнее задание добавлено')
         sqlite_connection.commit()
         cursor.close()
     except sqlite3.Error as error:
