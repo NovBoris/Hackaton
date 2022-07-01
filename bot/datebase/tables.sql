34369cd2203f377b8c81493527b9c356398431f2
MelekhJury <whitegold4864@gmail.com>
2022-07-02 01:10:47 +0300

    bla bla bla

--- bot/datebase/tables.sql
+++ bot/datebase/tables.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS done_homework(
 );
 
 CREATE TABLE IF NOT EXISTS check_homework(
-    id INTEGER PRIMARY KEY AUTOINCRIMENT,
+    id  INTEGER  PRIMARY KEY AUTOINCREMENT,
     photo TEXT NULL,
     username TEXT NOT NULL,
     userpass TEXT NOT NULL,
@@ -17,17 +17,17 @@ CREATE TABLE IF NOT EXISTS check_homework(
 );
 
 CREATE TABLE IF NOT EXISTS homework(
-    id INTEGER PRIMARY KEY AUTOINCRIMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    photo TEXT NULL,
+    photo TEXT NULL
 );
 
 CREATE TABLE IF NOT EXISTS accaunts(
     username TEXT NOT NULL PRIMARY KEY,
-    userpass TEXT NOT NULL,
+    userpass TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS admins(
     adminname TEXT NOT NULL PRIMARY KEY,
-    adminpass TEXT NOT NULL,
+    adminpass TEXT NOT NULL
 );
